92d087308056f05fb5a8c7266832e7fa12d61dd1
uynet <highsate@gmail.com>
2019-10-17 02:47:52 +0900

    moduleCSS

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-
-const s = {
-  logo: {
-    background: "#fff"
-  }
-};
+import s from "./style/home.module.scss";
+import { home } from "./style/home.module.scss";
 
 class Home extends React.Component {
   render() {
-    const logo = this.props.classes.logo;
+    console.log(home);
     return (
       <React.Fragment>
-        <div className={logo}>ゆいブログ</div>
+        <div className={home}>ゆいブログ</div>
       </React.Fragment>
     );
   }
