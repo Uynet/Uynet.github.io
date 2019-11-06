4c9381b93364519ba6665631aec24dcf85442a8c
uynet <highsate@gmail.com>
2019-11-06 16:36:42 +0900

    hamberが変化

--- src/components/hambarger.js
+++ src/components/hambarger.js
@@ -4,11 +4,21 @@ import { withStyles } from "@material-ui/core/styles";
 const s = {};
 
 class Hamberger extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+  onClick = e => {
+    this.setState({ isOpen: !this.state.isOpen });
+    this.props.onClick(this.state.isOpen);
+  };
   render() {
     console.log(this.ref);
     return (
       <React.Fragment>
-        <div onClick={this.props.onClick}>ぽ</div>
+        <div style={{ position: "absolute", top: 20, right: 40, zIndex: 10 }}>
+          <div onClick={this.onClick}>{this.state.isOpen ? "x" : "三"}</div>
+        </div>
       </React.Fragment>
     );
   }
