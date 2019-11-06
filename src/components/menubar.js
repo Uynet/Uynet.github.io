8c4164fbe8f213992e08bf874cabcfb79d318ebb
uynet <highsate@gmail.com>
2019-11-06 16:55:47 +0900

    wip:menu

--- src/components/menubar.js
+++ src/components/menubar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 
-const main = "#FF165C";
+const main = "#e2146a";
+const accent = "#FFDF6F";
 
 const width = 220;
 const s = {
@@ -20,9 +21,22 @@ const s = {
     position: "fixed",
     top: 0,
     left: "calc(100vw)"
+  },
+  itemtitle: {
+    color: accent,
+    fontSize: 30,
+    margin: 20
+  },
+  item: {
+    color: "#fff",
+    fontSize: 18,
+    padding: 12
   }
 };
 
+const createMenu = (name, link) => {
+  return { name: name, link: link };
+};
 // 右にでてくる
 class Menubar extends React.Component {
   constructor(props) {
@@ -33,12 +47,25 @@ class Menubar extends React.Component {
     this.setState({ isOpen: !this.state.isOpen });
   };
   render() {
+    const items = [
+      createMenu("Top", "/"),
+      createMenu("About", "/about"),
+      createMenu("Works", "/works")
+    ];
     const className = this.state.isOpen
       ? this.props.classes.open
       : this.props.classes.closed;
+    const itemTitleClass = this.props.classes.itemtitle;
     return (
       <React.Fragment>
-        <div className={className}></div>
+        <div className={className}>
+          <div className={itemTitleClass}>めにゅー</div>
+          <div>
+            {items.map((item, i) => {
+              return <div className={this.props.classes.item}>{item.name}</div>;
+            })}
+          </div>
+        </div>
       </React.Fragment>
     );
   }
