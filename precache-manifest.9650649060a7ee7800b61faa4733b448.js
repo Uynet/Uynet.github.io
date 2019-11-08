122bd6223deb7353b42f81059c57e1591cd8f890
uynet <highsate@gmail.com>
2019-11-08 13:38:29 +0900

    とりあえずbuildそのまま

---
+++ precache-manifest.9650649060a7ee7800b61faa4733b448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37af60bb8df34e407b439169f1f1fc92",
+    "url": "uynet.github.io/index.html"
+  },
+  {
+    "revision": "4bb471bc808b76ddc008",
+    "url": "uynet.github.io/static/css/main.1fa1c4f0.chunk.css"
+  },
+  {
+    "revision": "96a0b43c58af3d5ae7ab",
+    "url": "uynet.github.io/static/js/2.5cdceaef.chunk.js"
+  },
+  {
+    "revision": "4bb471bc808b76ddc008",
+    "url": "uynet.github.io/static/js/main.afa2497e.chunk.js"
+  },
+  {
+    "revision": "7249b245e08657f501f7",
+    "url": "uynet.github.io/static/js/runtime~main.b21ca656.js"
+  }
+]);
