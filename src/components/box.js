b07e589ad5099eae1d563496969f84a1bc9404b0
uynet <highsate@gmail.com>
2019-11-15 17:08:20 +0900

    BCN

--- src/components/box.js
+++ src/components/box.js
@@ -24,18 +24,17 @@ const s = {
   },
   title: {
     color: main,
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: "bold"
   },
   content: {
+    lineHeight: 1.4,
     //background: modalBG,
-    border: "dotted 3px " + accent,
+    //border: "dotted 3px " + accent,
     fontFamily: "honoka",
     borderRadius: 12,
     fontSize: 15,
-    paddingLeft: 15,
-    paddingTop: 8,
-    paddingBottom: 8
+    paddingLeft: 15
   }
 };
 
@@ -50,107 +49,148 @@ class Box extends React.Component {
         <div className={this.props.classes.frame}>
           <div className={about.bio}>{bio}</div>
           <div
-            className={this.props.classes.card}
-            style={{ animationDelay: this.getDelay() }}
+            style={{
+              opacity: 0,
+              animation: "fadeIn  ease 0.6s forwards",
+              animationDelay: this.getDelay(),
+              width: "80vw"
+            }}
           >
-            <div className={this.props.classes.title}>Profile</div>
-            <div className={this.props.classes.content}>
-              <table border="0">
-                <tbody>
-                  <tr>
-                    <th></th>
-                    <th></th>
-                  </tr>
-                  <tr>
-                    <td>BIRTH</td>
-                    <td></td>
-                    <td>1996/12/19</td>
-                  </tr>
-                  <tr>
-                    <td>ORG </td>
-                    <td></td>
-                    <td>東京工業大学/情報理工学院M1</td>
-                  </tr>
-                </tbody>
-              </table>
+            <div className={this.props.classes.card}>
+              <div className={this.props.classes.title}>Profile</div>
+              <div className={this.props.classes.content}>
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "auto 1fr",
+                    gridColumnGap: 8,
+                    alignItems: "center"
+                  }}
+                >
+                  <div>HN</div>
+                  <div>uynet(ゆいねっと)</div>
+                  <div>BIRTH</div>
+                  <div>1996/12/19</div>
+                  <div>ORG</div>
+                  <div>東京工業大学/情報理工学院M1</div>
+                </div>
+              </div>
             </div>
 
             <div className={this.props.classes.card}>
               <div className={this.props.classes.title}>実績等</div>
               <div className={this.props.classes.content}>
-                <table border="0">
-                  <tbody>
-                    <tr>
-                      <th></th>
-                      <th></th>
-                    </tr>
-                    <tr>
-                      <td>GoogleIGF2018</td>
-                      <td></td>
-                      <td>
-                        <a href="https://www.titech.ac.jp/news/2018/043127.html">
-                          Top10入賞
-                          <FontAwesomeIcon
-                            style={{ fontSize: 10 }}
-                            icon={["fas", "external-link-alt"]}
-                          ></FontAwesomeIcon>
-                        </a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>DeNA2018サマーインターン</td>
-                      <td></td>
-                      <td>ゲームエンジニアリングコース参加/優勝</td>
-                    </tr>
-                    <tr>
-                      <td>U22プログラミングコンテスト2018</td>
-                      <td></td>
-                      <td>
-                        <a href="https://www.titech.ac.jp/news/2019/043553.html">
-                          経済産業大臣賞(プロダクト)
-                          <FontAwesomeIcon
-                            style={{ fontSize: 10 }}
-                            icon={["fas", "external-link-alt"]}
-                          ></FontAwesomeIcon>
-                        </a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>ピクシブ株式会社</td>
-                      <td></td>
-                      <td>選考インターン参加</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "auto 1fr",
+                    gridColumnGap: 8,
+                    alignItems: "center"
+                  }}
+                >
+                  <div> GoogleIGF2018 </div>
+                  <div>
+                    <a href="https://www.titech.ac.jp/news/2018/043127.html">
+                      Top10入賞
+                      <FontAwesomeIcon
+                        style={{ fontSize: 10 }}
+                        icon={["fas", "external-link-alt"]}
+                      ></FontAwesomeIcon>
+                    </a>
+                  </div>
 
-              <div className={this.props.classes.card}>
-                <div className={this.props.classes.title}>できること</div>
-                <div className={this.props.classes.content}>
-                  ゲーム制作
-                  <br />
-                  webデザイン
-                  <br />
-                  作曲
-                  <br />
-                  JavaScript/TypeScript/NodeJS/
-                  <br />
-                  HTML/CSS/SCSS/React
-                  <br />
-                  PIXI.JS/webGL/OpenGL/GLSL/
-                  <br />
-                  Java/C++ vim
-                  <br />
+                  <div>DeNAサマーインターン2018 </div>
+                  <div>
+                    {" "}
+                    <a href="https://twitter.com/uynet/status/1034010674475585537">
+                      ゲームエンジニアリングコース参加/優勝
+                      <FontAwesomeIcon
+                        style={{ fontSize: 10 }}
+                        icon={["fas", "external-link-alt"]}
+                      ></FontAwesomeIcon>
+                    </a>
+                  </div>
+
+                  <div> U22プログラミングコンテスト2018</div>
+                  <div>
+                    <a href="https://www.titech.ac.jp/news/2019/043553.html">
+                      経済産業大臣賞(プロダクト)
+                      <FontAwesomeIcon
+                        style={{ fontSize: 10 }}
+                        icon={["fas", "external-link-alt"]}
+                      ></FontAwesomeIcon>
+                    </a>
+                  </div>
+                  <div></div>
+                  <div>
+                    <a href="https://www.bcnaward.jp/itjr/winning/date=2019">
+                      BCN ITジュニア賞2019
+                      <FontAwesomeIcon
+                        style={{ fontSize: 10 }}
+                        icon={["fas", "external-link-alt"]}
+                      ></FontAwesomeIcon>
+                    </a>
+                  </div>
+                  <div>ピクシブ株式会社</div>
+                  <div>選考インターン参加</div>
                 </div>
               </div>
-              <div className={this.props.classes.card}>
-                <div className={this.props.classes.title}>リンク</div>
-                <div className={this.props.classes.content}>
-                  <a href="https://www.pixiv.net/fanbox/creator/4180713">
-                    PIXIV FANBOX
-                  </a>
-                  <br />
-                </div>
+            </div>
+
+            <div className={this.props.classes.card}>
+              <div className={this.props.classes.title}>できること</div>
+              <div className={this.props.classes.content}>
+                ゲーム制作
+                <br />
+                webデザイン
+                <br />
+                作曲
+                <br />
+                JavaScript/TypeScript/NodeJS/
+                <br />
+                HTML/CSS/SCSS/React
+                <br />
+                PIXI.JS/webGL/OpenGL/GLSL/
+                <br />
+                Java/C++ vim
+                <br />
+              </div>
+            </div>
+            <div className={this.props.classes.card}>
+              <div className={this.props.classes.title}>リンク</div>
+              <div className={this.props.classes.content}>
+                <a href="http://github.com/Uynet">
+                  github
+                  <FontAwesomeIcon
+                    style={{ fontSize: 10 }}
+                    icon={["fas", "external-link-alt"]}
+                  ></FontAwesomeIcon>
+                </a>
+                <br />
+                <a href="http://twitter.com/uynet">
+                  twitter
+                  <FontAwesomeIcon
+                    style={{ fontSize: 10 }}
+                    icon={["fas", "external-link-alt"]}
+                  ></FontAwesomeIcon>
+                </a>
+                <br />
+                <a href="https://soundcloud.com/saihate-1">
+                  soundcloud
+                  <FontAwesomeIcon
+                    style={{ fontSize: 10 }}
+                    icon={["fas", "external-link-alt"]}
+                  ></FontAwesomeIcon>
+                </a>
+                <br />
+                <a href="https://www.pixiv.net/fanbox/creator/4180713">
+                  PIXIV FANBOX
+                  <FontAwesomeIcon
+                    style={{ fontSize: 10 }}
+                    icon={["fas", "external-link-alt"]}
+                  ></FontAwesomeIcon>
+                </a>
+                <br />
               </div>
             </div>
           </div>
